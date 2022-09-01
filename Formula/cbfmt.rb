9e055f5df30a987c3a04cf15a4080b17a644c11d
zegervdv <zegervdv@me.com>
2022-09-01 08:18:02 +0200

    update cbfmt 0.2.0

--- Formula/cbfmt.rb
+++ Formula/cbfmt.rb
@@ -1,8 +1,8 @@
 class Cbfmt < Formula
   desc "Tool to format codeblocks inside markdown and org documents"
   homepage "https://github.com/lukas-reineke/cbfmt"
-  url "https://github.com/lukas-reineke/cbfmt/archive/refs/tags/v0.1.1-1.tar.gz"
-  sha256 "8cb6b9d4602d9fa14e11bb31a09ec25b008e4a62401e6c31fe477b23fcaec144"
+  url "https://github.com/lukas-reineke/cbfmt/archive/refs/tags/v0.2.0.tar.gz"
+  sha256 "42973e9b1e95f4f3d7e72ef17a41333dab1e04d3d91c7930aabfc08f72c14126"
   license "MIT"
 
   bottle do
